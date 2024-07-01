0b0c28a9a93a25780597b0b4651e93fba4d19648
RushiKhose123 <rushikeshkhose123@gmail.com>
2024-07-01 12:36:58 +0530

    Login and signup logic done

---
+++ src/main/java/com/cabdriver/Cab/Driver/service/CustomerService.java
@@ -0,0 +1,31 @@
+package com.cabdriver.Cab.Driver.service;
+
+import com.cabdriver.Cab.Driver.exceptions.UserNotFound;
+import com.cabdriver.Cab.Driver.models.Customer;
+import com.cabdriver.Cab.Driver.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public void registerAccount(Customer customer){
+        customerRepository.save(customer);
+    }
+
+    public String authenticateUser(String email, String password){
+
+        Customer customer = customerRepository.findByEmail(email);
+        if(customer == null){
+            throw new UserNotFound(String.format("User with email %s does not exists", email));
+        }
+        String originalPassword = customer.getPassword();
+        if (originalPassword.equals(password)) {
+            return "Authentication Successfill";
+        }
+        return "Authentication Failed";
+    }
+}
